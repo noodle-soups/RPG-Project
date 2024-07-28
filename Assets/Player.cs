1894b15b12caa58e1dcf9ebc145c545ab3e2371a
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-07-28 15:10:20 +0100

    commit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,32 +8,65 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     private Animator anim;
 
-    [SerializeField] private float xInput;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
 
-    [SerializeField] private bool isMoving;
-    public float jumpForce;
+    private float xInput;
 
-    // Start is called before the first frame update
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xInput*moveSpeed, rb.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-
-        isMoving = rb.velocity.x != 0;
-        anim.SetBool("isMoving", isMoving);
-
+        Movement();
+        CheckInput();
+        FlipController();
+        AnimatorControllers();
     }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight) Flip();
+        else if (rb.velocity.x < 0 && facingRight) Flip();
+    }
+
+
+
 }
