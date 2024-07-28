41d9fc2e742e49f4ffe72be4a30b03a86ba1f630
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-07-28 09:52:22 +0100

    commit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float xInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 }
