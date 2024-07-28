555defcac3c3fe91ab4906a6ccb1f6673bfea872
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-07-28 14:39:09 +0100

    commit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    private Animator anim;
+
     [SerializeField] private float xInput;
     [SerializeField] private float moveSpeed;
+
+    [SerializeField] private bool isMoving;
     public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +31,9 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+
     }
 }
