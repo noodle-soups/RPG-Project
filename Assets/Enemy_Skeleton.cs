1cee302bf5926e3f90b82f7c470cee4f85bb7789
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-04 09:08:47 +0100

    commit

---
+++ Assets/Enemy_Skeleton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Skeleton : Entity
+{
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed; 
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    }
+
+}
